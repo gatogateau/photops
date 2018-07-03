15109301ebe268912ab4828a1067c133bece826b
chad green <chadgreen@chads-mbp.uconnect.utah.edu>
2018-07-02 19:05:32 -0600

    f

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -7,9 +7,8 @@ import Background from "../../images/Wallpaper-HD.jpg";
 const Jumbotron = () => (
 	<header className = "header">
 		<h4 id="userName">User Name</h4>
-		<row>
-		<h5>Kills<h5 id="kills">0</h5></h5>
-		</row>
+		<h5>Kills</h5>
+		<h5 id="kills">0</h5>
 		<h1 id="target">Target</h1>
 		<img id="targetpic" alt="target picture"></img>
 		
