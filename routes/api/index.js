c9df76f2462636f54b0edfa012df0de0efcdcc7c
chad green <chadgreen@chads-MacBook-Pro.local>
2018-07-05 14:45:54 -0600

    ghetto login

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,13 @@ const path= require("path");
 const router = require("express").Router();
 const userRoutes = require("./users");
 const gamesRoutes = require("./games");
+const accountRoutes = require("./signin")
 
 // User routes
 router.use("/users", userRoutes);
 
 // Game routes  
 router.use("/games", gamesRoutes);
+
+router.use("./account", accountRoutes)
 module.exports = router;
