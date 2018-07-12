28e01c859d943fc2f307f5335077926359eb5a9f
Jerson Hatch <vote4chubs@hotmail.com>
2018-07-12 12:46:34 -0600

    updates to routes

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -14,71 +14,82 @@ module.exports = {
     // startGame
 
 
-// find all games active and inactive and return game name and allPlayers
+    // find all games active and inactive and return game name and allPlayers
 
-	findAllGameName: function (req, res) {
+    
+    
+    findAllGameName: function (req, res) {
         db.Games
-        
-        .find(req.query)
-        // .select ({"game":1, "allPlayers":1, "_id":0 })
-        // .sort({ date: -1 })
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
+
+            .find(req.query)
+            .select ({"game":1, "allPlayers":1, "_id":0 })
+            // .sort({ date: -1 })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
     },
+
 
     // Get all the Games with all the information
     findAllGames: function (req, res) {
+        console.log("find all games?")
         db.Games
-        .find(req.query)
-        .then(allGamesRes => res.json (allGamesRes))
-        .catch(err => res.status(422).json(err));
+
+            .find(req.query)
+            .then(whatever => res.json(whatever ))
+            .catch(err => res.status(422).json(err));
     },
 
     // take user._id and return activeGames
-    findUserGames: function(req, res) {
+    findUserGames: function (req, res) {
         console.log(req.session.passport.user)
         db.User
-            .findById({ "_id": req.session.passport.user._id })
+            .findById({
+                "_id": req.session.passport.user._id
+            })
             .then(user => {
                 console.log(user.activeGames)
                 res.json(user)
             })
     },
-    
-    
-	findById: function (req, res) {
+
+
+    findById: function (req, res) {
         console.log("hit")
-		db.Games
-			.findById(req.params.id)
-			.then(dbModel => res.json(dbModel))
-			.catch(err => res.status(422).json(err));
+        db.Games
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
     },
-    
+
     // create new Game
-	createGame: function (req, res) {
+    createGame: function (req, res) {
         console.log(req.body);
-		db.Games
-			.create(req.body)
-			.then(dbModel => res.json(dbModel))
-			.catch(err => res.status(422).json(err));
-	},
-	update: function (req, res) {
-		db.Games
-			.findOneAndUpdate({ _id: req.params.id }, req.body)
-			.then(dbModel => res.json(dbModel))
-			.catch(err => res.status(422).json(err));
-	},
-	remove: function (req, res) {
-		db.Games
-			.findById({ _id: req.params.id })
-			.then(dbModel => dbModel.remove())
-			.then(dbModel => res.json(dbModel))
-			.catch(err => res.status(422).json(err));
+        db.Games
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    update: function (req, res) {
+        db.Games
+            .findOneAndUpdate({
+                _id: req.params.id
+            }, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    remove: function (req, res) {
+        db.Games
+            .findById({
+                _id: req.params.id
+            })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
     },
 
     // create game 
     createGame: function (req, res) {
-        db.Games    
+        db.Games
             .create(req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
@@ -87,8 +98,10 @@ module.exports = {
     // find a game by game id, add username to allPlayers
     // We may not need this one because joinGameByGameName
     joinGame: function (req, res) {
-        db.Games   
-            .findOneAndUpdate({ game: req.params.game}, req.body)
+        db.Games
+            .findOneAndUpdate({
+                game: req.params.game
+            }, req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
@@ -106,42 +119,58 @@ module.exports = {
     //         .catch(err => res.status(422).json(err));
     // },
 
-       //  API route created.  
+    //  API route created.  
     // add user to game. After user searches for game, button to add them to game.  If they are already in the game, res.message "you are already in the game", if res.data - post data 
     joinGameByGameName: function (req, res) {
         console.log(req.body)
         db.Games
-        .find({"game": req.body.game})
-        .select({"game":1, "playersAlive":1, "playersDead":1, "game":1, "allPlayers":1})
-        // 
-        .then(gameData => {
-            // gameData.allPlayers.push(req.session.userName)
+            .find({
+                "game": req.body.game
+            })
+            .select({
+                "game": 1,
+                "playersAlive": 1,
+                "playersDead": 1,
+                "game": 1,
+                "allPlayers": 1
+            })
+            // 
+            .then(gameData => {
+                // gameData.allPlayers.push(req.session.userName)
 
-            console.log(gameData[0])
-            // if already in game, do not add to game data
+                console.log(gameData[0])
+                // if already in game, do not add to game data
 
-            if (gameData[0].allPlayers.includes(req.session.passport.user.userName)) {
-                res.json({message:"You are already in the game"})
-                
-            } else {
+                if (gameData[0].allPlayers.includes(req.session.passport.user.userName)) {
+                    res.json({
+                        message: "You are already in the game"
+                    })
 
-                gameData[0].allPlayers.push(req.session.passport.user.userName)
-                db.Games
-                .findOneAndUpdate({"game":req.body.game},{"allPlayers":gameData[0].allPlayers})
-                .then(newGameData => {
-                    console.log ("this is newGameData", newGameData);
-                    res.json(gameData)
-                })
-            }
-        })
-        .catch(err => res.status(422).json(err));
+                } else {
+
+                    gameData[0].allPlayers.push(req.session.passport.user.userName)
+                    db.Games
+                        .findOneAndUpdate({
+                            "game": req.body.game
+                        }, {
+                            "allPlayers": gameData[0].allPlayers
+                        })
+                        .then(newGameData => {
+                            console.log("this is newGameData", newGameData);
+                            res.json(gameData)
+                        })
+                }
+            })
+            .catch(err => res.status(422).json(err));
     },
     // start the game - find game by id, then copy all players to playersAlive
 
     // this doesn't work yet
     startGame: function (req, res) {
         db.Games
-            .findById({ _id: req.params.id })
+            .findById({
+                _id: req.params.id
+            })
 
             .then(dbModel => {
                 // change arrays
@@ -160,7 +189,9 @@ module.exports = {
     // if a player is assassinated, remove name from playersAlive and add to playersDead.  Finds by id, requests userName, 
     playerDie: function (req, res) {
         db.Games
-            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .findOneAndUpdate({
+                _id: req.params.id
+            }, req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
@@ -168,12 +199,14 @@ module.exports = {
     // player assassinates the target, target gets removed from playersAlive in db.Games
     playerKillsTarget: function (req, res) {
         db.Games
-            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .findOneAndUpdate({
+                _id: req.params.id
+            }, req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.stats(422).json(err));
     }
 
-    
+
 
 
 
