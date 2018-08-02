86496bed5d8dd3e47d248f50cce740c80c8f8eae
chad green <chadgreen@chads-MacBook-Pro.local>
2018-08-02 17:01:26 -0600

    trying to get the jumbotron to look better on mobile

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -7,12 +7,11 @@ import "./Jumbotron.css";
 const Jumbotron = (props) => (
 	<header className = "header">
 	<h1 id="userName">{props.username.toUpperCase()}</h1>
-	<div className="gradient">
 		<h5 id="kills">Kills : {props.kills}</h5>
 		<h3 id="currentGame">Game: {props.currentGame}</h3>
 		<h3 id="target">Target : <span id="tguy">{props.target}</span></h3>
 		<img src={props.targetURL}id="targetpic" alt="Target needs to upload their face!"></img>
-		</div>
+		
 		
 	</header>
 );
