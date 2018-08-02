3404a3b8d065d54e500976dc00030cbafaade1df
chad green <chadgreen@chads-MacBook-Pro.local>
2018-08-02 13:03:39 -0600

    trying something

--- client/src/pages/Verifycam/Verifycam2.js
+++ client/src/pages/Verifycam/Verifycam2.js
@@ -1,56 +1,157 @@
 import React, { Component } from 'react';
-// import { Text, View, TouchableOpacity } from 'react-native';
-import { Camera, Permissions } from 'expo';
+import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
+import $ from "jquery";
+import cloudinary from 'cloudinary';
+import axios from 'axios';
+import Modal from 'react-awesome-modal';
+ 
+class Verifycam2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        visible1: false,
+        visible2: false,
+        visible3: false,
+        currentGame: this.props.currentGame,
+    };
+    this.runKillFunction = this.runKillFunction.bind(this);
+}
 
-export default class Verifycam extends Component {
-  state = {
-    hasCameraPermission: null,
-    type: Camera.Constants.Type.back,
-  };
+  onTakePhoto (dataUri) {
+    let that = this;
+    // Do stuff with the dataUri photo...
+    console.log(dataUri);
+    cloudinary.uploader.upload(dataUri, function (result) {
+      console.log(result);
+      let payload = { "image": result.url, "subject_id": that.props.target, "gallery_name": "players" };
+      let headers = {
+        "Content-type": "application/json",
+        "app_id": "3152266b",
+        "app_key": "702449259a83518d27467f43d20715d4"
+      };
+      $.ajax("https://api.kairos.com/verify", {
+          headers: headers,
+          type: "POST",
+          data: JSON.stringify(payload),
+          dataType: "text"
+      }).done(function (response) {
+        console.log(response);
+          let confidence = JSON.parse(response).images["0"].transaction.confidence;
 
-  async componentWillMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
+          if(confidence>=.80) {
+            that.openModal1();
+            that.runKillFunction();
+          } else {
+            that.openModal2();
+          // change alert to modal
+        } 
+          
+      });
+
+  });
+  }
+ 
+  onCameraError (error) {
+    console.error('onCameraError', error);
   }
 
-  render() {
-    const { hasCameraPermission } = this.state;
-    if (hasCameraPermission === null) {
-      return <view />;
-    } else if (hasCameraPermission === false) {
-      return <p>No access to camera</p>;
-    } else {
-      return (
-        <view style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
-            <view
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}>
-              <button
-                style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={() => {
-                  this.setState({
-                    type: this.state.type === Camera.Constants.Type.back
-                      ? Camera.Constants.Type.front
-                      : Camera.Constants.Type.back,
-                  });
-                }}>
-                <p
-                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                  {' '}Flip{' '}
-                </p>
-              </button>
-            </view>
-          </Camera>
-        </view>
-      );
-    }
+  openModal1() {
+    this.setState({
+        visible1: true
+    });
+}
+
+closeModal1() {
+    this.setState({
+        visible1: false
+    });
+}
+openModal2() {
+    this.setState({
+        visible2: true
+    });
+}
+
+closeModal2() {
+    this.setState({
+        visible2: false
+    });
+}
+openModal3() {
+    this.setState({
+        visible3: true
+    });
+}
+
+closeModal3() {
+    this.setState({
+        visible3: false
+    });
+}
+
+runKillFunction() {
+    let that = this;
+    console.log("pre kill target");
+    axios.put('/api/games/killTarget', {
+        username: that.props.target,
+        game: that.state.currentGame
+
+    })
+      .then(function (response) {
+        // handle success
+        console.log(response.data);
+        if(response.data == "Chad Rules"){
+            that.openModal3(); 
+        }
+
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  }
+ 
+  render () {
+    return (
+      <div className="Verifycam2">
+        <Camera
+          onTakePhoto = { (dataUri) => { this.onTakePhoto(dataUri); } }
+          onCameraError = { (error) => { this.onCameraError(error); } }
+          idealFacingMode = {FACING_MODES.ENVIRONMENT}
+          idealResolution = {{width: 640, height: 480}}
+          imageType = {IMAGE_TYPES.JPG}
+          imageCompression = {0.97}
+          isMaxResolution = {false}
+          isImageMirror = {false}
+          isDisplayStartCameraError = {true}
+          sizeFactor = {1}
+        />
+      
+      <Modal visible={this.state.visible1} width="400" height="300" effect="fadeInUp" onClickAway={() => this.closeModal1()}>
+        <div>
+          <h1 className="eliminated">Target Eliminated!</h1>
+          <a href="javascript:void(0);" onClick={() => this.closeModal1()}>Close</a>
+        </div>
+      </Modal>
+      <Modal visible={this.state.visible2} width="400" height="300" effect="fadeInDown" onClickAway={() => this.closeModal2()}>
+        <div>
+          <h1 className="missed">Oops, you missed!</h1>
+          <p>Please try again.</p>
+          <a href="javascript:void(0);" onClick={() => this.closeModal2()}>Close</a>
+        </div>
+      </Modal>
+      <Modal visible={this.state.visible3} width="400" height="300" effect="fadeInUp" onClickAway={() => this.closeModal3()}>
+        <div>
+          <h1 className="youWin">Congratulations</h1>
+          <img id="winning" src="https://media0.giphy.com/media/Q56SF4czEtSZG/giphy.gif"/>
+          <h1 className="win">You Win!</h1>
+          <a href="javascript:void(0);" onClick={() => this.closeModal3()}>Close</a>
+        </div>
+      </Modal>
+      </div>
+    );
   }
 }
+ 
+export default Verifycam2;
