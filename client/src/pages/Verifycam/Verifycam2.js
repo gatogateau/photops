0e95144521200434f47f715beddc7b83c6e228e7
chad green <chadgreen@chads-MacBook-Pro.local>
2018-08-02 17:27:32 -0600

    broke it

--- client/src/pages/Verifycam/Verifycam2.js
+++ client/src/pages/Verifycam/Verifycam2.js
@@ -23,7 +23,7 @@ class Verifycam2 extends Component {
         currentGame: this.props.currentGame,
     };
     this.runKillFunction = this.runKillFunction.bind(this);
-    this.onTakePhoto = this.onTakePhoto.bind(this)
+    this.onTakePhoto = this.onTakePhoto.bind(this);
 }
 
   onTakePhoto (dataUri) {
