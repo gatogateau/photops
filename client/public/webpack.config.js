b1f62dd6594307d8328361a6682079ed4f51cd15
chad green <chadgreen@chads-MacBook-Pro.local>
2018-08-05 10:38:06 -0600

    webpack config

---
+++ client/public/webpack.config.js
@@ -0,0 +1,7 @@
+devServer: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+},
